c79be3d77123748f5b91a5b833c18aa0228ca5d6
lazamar <lazaronijunior@gmail.com>
2016-11-13 18:12:26 +0000

    Add environment variables check

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 // Load environment variables
 require('dotenv').config();
 
+// Check environment variables:
+const assert = require('assert');
+assert(process.env.DOMAIN, 'No domain specified in .env file');
+assert(process.env.DB_NAME, 'No database name specified in .env file');
+assert(process.env.CONSUMER_KEY, 'No Twitter consumer key specified in .env file');
+assert(process.env.CONSUMER_SECRET, 'No Twitter consumer secret specified in .env file');
+
 module.exports = require('./server');
